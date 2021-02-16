ac89ce2ac3822e6482f42ad0bf2c6291740f5ecf
lnewmanheggie <lnewmanheggie@gmail.com>
2021-02-16 16:32:02 -0600

    map working on local-data hbs page

--- public/js/localMapData.js
+++ public/js/localMapData.js
@@ -3,8 +3,15 @@ $(document).ready(initMap)
 function initMap() {
 
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 39.8283, lng: -98.5795 },
-    zoom: 4,
+    center: { lat: 39.742043, lng: 	-104.991531 },
+    zoom: 8,
   });
+
+    // const latLng = new google.maps.LatLng(39.742043, -104.991531);
+  // new google.maps.Marker({
+  //   position: latLng,
+  //   label: "A",
+  //   map: map,
+  // });
 
 }
