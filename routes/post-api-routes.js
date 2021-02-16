4a4f1c6b7d3298a519b974c1ed8121b69cfe3b26
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-02-16 11:21:37 -0600

    Added Get routes for user input

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 
 module.exports = function(app) {
     // POST route for saving a new report
-    app.post("/api/userReport", function(req, res) { //make reports table later
+    app.post("/api/userReport", function(req, res) {
         db.Report.create({
             category: req.body.category, //drop down input??
             streetAddress: req.body.streetAddress,
@@ -17,44 +17,34 @@ module.exports = function(app) {
             res.json(dbpost);
         })
     });
-    //   app.get("/api/TABLENAME", function(req, res) {
-    //     var query = {};
-    //     if (req.query.crime_id) {
-    //       query.CrimeId = req.query.crime_id;
-    //     }
 
-    //     db.Post.findAll({
-    //       where: query,
-    //       include: [db.??]
-    //     }).then(function(??) {
-    //       res.json(??);
-    //     });
-    //   });
+    app.get("/api/userReport", function(req, res) {
+        db.Report.findAll({}).then(function(dbPost) {
+            res.json(dbPost);
+        });
 
-    //   app.get("/api/TABLENAME/:id", function(req, res) {
-    //     // Here we add an "include" property to our options in our findOne query
-    //     // We set the value to an array of the models we want to include in a left outer join
-    //     // In this case, just db.Author
-    //     db.Post.findOne({
-    //       where: {
-    //         id: req.params.id
-    //       },
-    //       include: [db.??]
-    //     }).then(function(??) {
-    //       res.json(??);
-    //     });
-    //   });
+    });
 
-    //   // DELETE route for deleting posts
-    //   app.delete("/api/TABLENAME/:id", function(req, res) {
-    //     db.??.destroy({
-    //       where: {
-    //         id: req.params.id
-    //       }
-    //     }).then(function(??) {
-    //       res.json(??);
-    //     });
-    //   });
+    app.get("/api/userReport/:id", function(req, res) {
+        db.Report.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
+
+    // DELETE route for deleting posts
+    app.delete("/api/userReport/:id", function(req, res) {
+        db.Report.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
 
 
 };
