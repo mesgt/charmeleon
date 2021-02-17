5b8e5bba2ad9bfc143645da0ec1fc0f51de19f96
fongvang09 <koichi_vang@hotmail.com>
2021-02-17 13:12:08 -0600

    3rd page layout

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 // const { config } = require('dotenv/types')
+require('dotenv').config();
 
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "root",
+    "username": process.env.DEVELOPMENT_USERNAME,
+    "password": process.env.DEVELOPMENT_PASSWORD,
     "database": "crimeAde",
     "host": "127.0.0.1",
     "port": 3306,
