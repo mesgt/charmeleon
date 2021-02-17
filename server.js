90885a197921e872f5dc1b690ad1a809e94dface
Maria Sargent <maria.marshall.83@gmail.com>
2021-02-17 12:29:16 -0600

    Added jawsdb.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ require("./routes/localReports.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
