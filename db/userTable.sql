612c94a14920d22143f756fd2204e95e32b241c4
Maria Sargent <maria.marshall.83@gmail.com>
2021-02-17 11:06:40 -0600

    Updated userTable.sql to match new input order.

--- db/userTable.sql
+++ db/userTable.sql
@@ -2,11 +2,11 @@ CREATE TABLE userReport
 (
 	id int NOT NULL AUTO_INCREMENT,
 	category text NOT NULL, -- from drop down menu
+    date DATE NOT NULL,
     streetAddress VARCHAR(35) NOT NULL,
     city VARCHAR(35),
     state VARCHAR(20),
     zip INT(9) NOT NULL,
-    date DATE NOT NULL, 
     description VARCHAR(255),
 	PRIMARY KEY (id)
 );
