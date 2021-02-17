675221f1085f389ebf8ede1c04d279f40213987a
Will Heyer <williamhheyer@gmail.com>
2021-02-17 15:26:35 -0600

    Fixed merge Conflicts

--- server.js
+++ server.js
@@ -27,11 +27,7 @@ require("./routes/localReports.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-<<<<<<< HEAD
-db.sequelize.sync().then(function() {
-=======
 db.sequelize.sync({}).then(function() {
->>>>>>> f55ed9497378c89e3c2af098054576790f33dd58
   app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
